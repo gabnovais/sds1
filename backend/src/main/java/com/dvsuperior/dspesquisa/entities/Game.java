0b479f43bedad6c6945b271c11719e372ecd6f4c
Gabriel Novais <gsnbil@gmail.com>
2020-09-16 19:29:22 -0300

    DTO, Service

--- backend/src/main/java/com/dvsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/dvsuperior/dspesquisa/entities/Game.java
@@ -17,8 +17,8 @@ import com.dvsuperior.dspesquisa.entities.enums.Platform;
 
 @Entity
 @Table(name="tb_game")
-public class Game implements Serializable {	
-	private static final long serialVersionUID = 1L;
+	public class Game implements Serializable {	
+		private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,10 +26,12 @@ public class Game implements Serializable {
 	private String title;
 	private Platform platform;
 
+
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
 	
+
 	@OneToMany(mappedBy = "game")
 	private List<Record> records = new ArrayList<>(); 
 
